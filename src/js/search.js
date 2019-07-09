d99e04b9530a219b55bff2884300b127a9a0100f
Sergii Pushkovskyi <Sergii.Pushkovskyi@infopulse.com>
2019-07-09 16:46:21 +0300

    Finished working on hw

--- src/js/search.js
+++ src/js/search.js
@@ -6,8 +6,13 @@ const searchForm = document.getElementById('search-form');
 const imagesList = document.getElementById('gallery');
 const searchQueryButton = document.getElementById('search-query');
 const elem = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const overlayImg = document.querySelector('.overlay img');
+const closeModal = document.querySelector('[data-action="close-modal"]');
 
 searchForm.addEventListener('submit', searchFormHandler);
+imagesList.addEventListener('click', handleClick);
+overlay.addEventListener('click', handleBackdrop);
 
 searchQueryButton.addEventListener('click', loadMoreHandler);
 
@@ -27,6 +32,7 @@ function searchFormHandler(event) {
     imagesList.insertAdjacentHTML('beforeend', markup);
     searchForm.elements.query.value = '';
   });
+  searchQueryButton.disabled = false;
 }
 
 function loadMoreHandler() {
@@ -34,6 +40,40 @@ function loadMoreHandler() {
     const markup = buildImageTemplate(data.hits);
     imagesList.insertAdjacentHTML('beforeend', markup);
   });
+}
+
+function handleClick(event) {
+  const target = event.target;
+  if (target.tagName !== 'IMG') {
+    return;
+  }
+  console.dir(target);
+  overlayImg.src = event.target.currentSrc;
+  overlayImg.alt = event.target.alt;
+  overlay.classList.add('is-visible');
+  closeModal.addEventListener('click', handleCloseModal);
+  window.addEventListener('keydown', handleKeyPress);
+}
+
+function handleCloseModal() {
+  overlay.classList.remove('is-visible');
+  overlayImg.src = '';
+  overlayImg.alt = '';
+  window.removeEventListener('keydown', handleKeyPress);
+}
+
+function handleBackdrop(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  handleCloseModal();
+}
+
+function handleKeyPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  handleCloseModal();
 }
 
 infScroll.on('scrollThreshold', loadMoreHandler);
